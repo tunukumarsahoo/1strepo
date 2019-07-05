b2c32f76be01bd75fe62554063330ba595fa3096
root <root@ip-172-31-30-115.ap-south-1.compute.internal>
2019-07-05 04:56:50 +0000

    class added - :

---
+++ sahjava.java
@@ -0,0 +1,12 @@
+public class sahjava {
+
+	public static void main(String[] args) {
+		System.out.println("hello world in git");
+		for(int i=0 ;i<10;i++)
+		{
+			System.out.println(i);
+			
+		}
+	}
+
+}
